ae04b0d258bba45e294cfe0366054d6af0898f7e
LucasStarlingdePaulaSalles <lucastarling1@gmail.com>
2021-07-22 17:28:58 -0300

    dev: real distance max heuristic (3/2 score)

--- search.py
+++ search.py
@@ -221,52 +221,15 @@ def aStarSearch(problem, heuristic=nullHeuristic):
     return []
 
 def foodHeuristic(state, problem):
-    """
-    Your heuristic for the FoodSearchProblem goes here.
+    position, food_grid = state
+    goals = food_grid.asList()
+    max_ = 0
 
-    This heuristic must be consistent to ensure correctness.  First, try to come
-    up with an admissible heuristic; almost all admissible heuristics will be
-    consistent as well.
+    for goal in goals:
+        dist = aux_bfs(position,problem.walls,goal)
+        if dist > max_: max_ = dist
 
-    If using A* ever finds a solution that is worse uniform cost search finds,
-    your heuristic is *not* consistent, and probably not admissible!  On the
-    other hand, inadmissible or inconsistent heuristics may find optimal
-    solutions, so be careful.
-
-    The state is a tuple ( pacmanPosition, foodGrid ) where foodGrid is a Grid
-    (see game.py) of either True or False. You can call foodGrid.asList() to get
-    a list of food coordinates instead.
-
-    If you want access to info like walls, capsules, etc., you can query the
-    problem.  For example, problem.walls gives you a Grid of where the walls
-    are.
-
-    If you want to *store* information to be reused in other calls to the
-    heuristic, there is a dictionary called problem.heuristicInfo that you can
-    use. For example, if you only want to count the walls once and store that
-    value, try: problem.heuristicInfo['wallCount'] = problem.walls.count()
-    Subsequent calls to this heuristic can access
-    problem.heuristicInfo['wallCount']
-    """
-    position, foodGrid = state
-    goals = foodGrid.asList()
-    sum_ = 0
-
-    point = position
-    while len(goals) > 0:
-        min_ = 10000000
-
-        for goal in goals:
-            # dist = aux_bfs(point,problem.walls,goal)
-            dist = abs(point[0] - goal[0]) + abs(point[1] - goal[1])
-            if dist < min_:
-                min_ = dist
-                next_ = goal
-        sum_ += min_
-        goals.remove(next_)
-        point = next_
-
-    return sum_
+    return max_
 
 
 # Abbreviations
